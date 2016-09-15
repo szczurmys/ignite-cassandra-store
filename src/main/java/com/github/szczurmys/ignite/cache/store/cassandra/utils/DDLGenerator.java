b0bdffd1932a82b6f4f2520d45dea9e92a380a3a
Igor <irudyak@gmail.com>
2016-09-15 09:41:22 +0700

    IGNITE-3791 Wrapped table name, keyspace name and column name in all generated DDL/DML statements. - Fixes #1062.
    Signed-off-by: Alexey Kuznetsov <akuznetsov@apache.org>

--- src/main/java/com/github/szczurmys/ignite/cache/store/cassandra/utils/DDLGenerator.java
+++ src/main/java/com/github/szczurmys/ignite/cache/store/cassandra/utils/DDLGenerator.java
@@ -18,6 +18,8 @@
 package com.github.szczurmys.ignite.cache.store.cassandra.utils;
 
 import java.io.File;
+import java.util.List;
+
 import com.github.szczurmys.ignite.cache.store.cassandra.persistence.KeyValuePersistenceSettings;
 
 /**
@@ -52,6 +54,14 @@ public class DDLGenerator {
                 System.out.println();
                 System.out.println(settings.getTableDDLStatement());
                 System.out.println();
+
+                List<String> statements = settings.getIndexDDLStatements();
+                if (statements != null && !statements.isEmpty()) {
+                    for (String st : statements) {
+                        System.out.println(st);
+                        System.out.println();
+                    }
+                }
             }
             catch (Throwable e) {
                 System.out.println("-------------------------------------------------------------");
