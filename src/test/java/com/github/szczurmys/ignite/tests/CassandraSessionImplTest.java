b6a9949904eb7c15d68c596a69062fadf8aba2e4
Jakub Goszczurny <szczurmys@o2.pl>
2018-06-10 19:03:45 +0200

    IGNITE-7397: Fix cache configuration and reduced trainings count in MLP group training test.

--- src/test/java/com/github/szczurmys/ignite/tests/CassandraSessionImplTest.java
+++ src/test/java/com/github/szczurmys/ignite/tests/CassandraSessionImplTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package com.github.szczurmys.ignite.tests;
 
 import static org.junit.Assert.assertEquals;
